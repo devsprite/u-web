b7b1509e4630e2c57ac89b67ed4917c2a58f2ac9
Dominique <dominique.lopez@digitregroup.com>
2020-06-01 07:32:35 +0200

    wip refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'test', age: 314 });
-user.save();
-user.events.on('click', () => {
-  console.log('Click');
-});
-user.events.trigger('click');
+
+console.log(user.get('name'));
